01482a8817ea4bc6ce13b8d8aa4321cfc895dcf2
Julie Qiu <julieyeqiu@gmail.com>
2017-03-01 10:18:48 -0500

    removed irbrc and added setup.sh

--- irbrc
+++
@@ -1,2 +0,0 @@
-require 'irbtools'
-require 'irbtools/more'
---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/zprofile ~/.zprofile
+ln -s ~/dotfiles/zpreztorc ~/.zpreztorc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/vim ~/.vim
