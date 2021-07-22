de7bcce98d9a68029a0ce995d3c272a742cfa95b
Julie Qiu <julie@golang.org>
2021-07-22 12:46:27 -0400

    scripts: update setup_dotfiles.sh with gitconfig and tmux

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -1,3 +1,5 @@
-ln -s $HOME/dotfiles/bashrc ~/.bashrc
 ln -s $HOME/dotfiles/zshrc ~/.zshrc
 ln -s $HOME/dotfiles/vimrc ~/.vimrc
+ln -s $HOME/dotfiles/gitconfig ~/.gitconfig
+ln -s $HOME/dotfiles/p10k.zsh ~/.p10k.zsh
+ln -s $HOME/dotfiles/tmux ~/.tmux
