921b5b9902a28c432b185a201bd9a4dab9f4a140
Julie Qiu <julie@golang.org>
2021-07-23 10:47:03 -0400

    scripts/setup_dotfiles.sh: update to remove dotfiles if exists

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -1,3 +1,8 @@
+rm -f $HOME/.gitconfig
+rm -f $HOME/.p10k.zsh
+rm -f $HOME/.vimrc
+rm -f $HOME/.zshrc
+rm -f $HOME/.tmux
 ln -s $HOME/dotfiles/config/gitconfig ~/.gitconfig
 ln -s $HOME/dotfiles/config/p10k.zsh ~/.p10k.zsh
 ln -s $HOME/dotfiles/config/tmux ~/.tmux
