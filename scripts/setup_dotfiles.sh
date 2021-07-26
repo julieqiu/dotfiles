e4158b7f7de7c09fbec50558624f869372c431e9
Julie Qiu <julie@golang.org>
2021-07-26 19:40:00 -0400

    scripts/setup_dotfiles.sh: add tmux.conf

--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -3,8 +3,9 @@ rm -f $HOME/.p10k.zsh
 rm -f $HOME/.vimrc
 rm -f $HOME/.zshrc
 rm -f $HOME/.tmux
+rm -f $HOME/.tmux.conf
 ln -s $HOME/dotfiles/config/gitconfig ~/.gitconfig
 ln -s $HOME/dotfiles/config/p10k.zsh ~/.p10k.zsh
-ln -s $HOME/dotfiles/config/tmux ~/.tmux
+ln -s $HOME/dotfiles/config/tmux.conf ~/.tmux.conf
 ln -s $HOME/dotfiles/config/vimrc ~/.vimrc
 ln -s $HOME/dotfiles/config/zshrc ~/.zshrc
