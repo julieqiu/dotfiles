f0a82f383913236f3ed157161e6dbe7620312bc7
Julie Qiu <julie@golang.org>
2021-10-23 10:42:58 -0400

    config,scripts: use oh-my-zsh to manage zsh plugins

--- install/brew.sh
+++ install/brew.sh
@@ -18,9 +18,7 @@ brew install \
 	tree \
 	vim \
 	watch \
-	wget \
-	zsh-history-substring-search \
-	zsh-syntax-highlighting
+	wget
 
 # Install bash to
 # [fix this](https://apple.stackexchange.com/questions/291287/globstar-invalid-shell-option-name-on-macos-even-with-bash-4-x)
--- scripts/setup_dotfiles.sh
+++ scripts/setup_dotfiles.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 rm -f $HOME/.gitconfig
 rm -f $HOME/.p10k.zsh
 rm -f $HOME/.vimrc
@@ -10,3 +9,7 @@ ln -s $HOME/dotfiles/config/p10k.zsh ~/.p10k.zsh
 ln -s $HOME/dotfiles/config/tmux.conf ~/.tmux.conf
 ln -s $HOME/dotfiles/config/vimrc ~/.vimrc
 ln -s $HOME/dotfiles/config/zshrc ~/.zshrc
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
